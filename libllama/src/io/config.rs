29c011bbfddd8ae616f443f5cf57db6eeb2151f6
archshift <gh@archshift.com>
2017-01-21 09:15:46 +0000

    Fix SDMMC registers, add CONFIG registers

--- libllama/src/io/config.rs
+++ libllama/src/io/config.rs
@@ -1,1 +1,10 @@
-iodevice!(ConfigDevice, {});
+iodevice!(ConfigDevice, {
+    0x000 => sysprot9: u8 { }
+    0x001 => sysprot11: u8 { }
+    0x002 => reset11: u8 { }
+    0x004 => debugctl: u32 { }
+    0x00C => cardctl: u16 { }
+    0x010 => cardstatus: u8 { }
+    0x012 => cardcycles0: u16 { }
+    0x014 => cardcycles1: u16 { }
+});
--- libllama/src/io/emmc.rs
+++ libllama/src/io/emmc.rs
@@ -10,34 +10,33 @@ bitfield!(RegCmd: u16, {
 iodevice!(EmmcDevice, {
     0x000 => cmd: u16 { }
     0x002 => port_select: u16 { }
-    0x004 => response0: u16 { write_bits = 0; }
-    0x006 => response1: u16 { write_bits = 0; }
-    0x008 => response2: u16 { write_bits = 0; }
-    0x00A => response3: u16 { write_bits = 0; }
-    0x00C => response4: u16 { write_bits = 0; }
-    0x00E => response5: u16 { write_bits = 0; }
-    0x010 => response6: u16 { write_bits = 0; }
-    0x012 => response7: u16 { write_bits = 0; }
-    // TODO: these registers below
-    0x014 => reg_response4: u16 { }
-    0x016 => reg_response5: u16 { }
-    0x018 => reg_response6: u16 { }
-    0x01A => reg_response7: u16 { }
-    0x01C => reg_irq_status0: u16 { }
-    0x01E => reg_irq_status1: u16 { }
-    0x020 => reg_irq_mask0: u16 { }
-    0x022 => reg_irq_mask1: u16 { }
-    0x024 => reg_clk_ctl: u16 { }
-    0x026 => reg_data16_blk_len: u16 { }
-    0x028 => reg_card_option: u16 { }
-    0x02C => reg_err_status0: u16 { }
-    0x02E => reg_err_status1: u16 { }
-    0x030 => reg_data16_fifo: u16 { }
-    0x0D8 => reg_data16_ctl: u16 { }
-    0x0E0 => reg_software_reset: u16 { write_bits = 0b1; }
-    0x0F6 => reg_protected: u16 { }
-    0x100 => reg_data32_ctl: u16 { }
-    0x104 => reg_data32_blk_len: u16 { }
-    0x108 => reg_data32_blk_cnt: u16 { }
-    0x10C => reg_data32_fifo: u16 { }
+    0x004 => param0: u16 { }
+    0x006 => param1: u16 { }
+    0x008 => stop: u16 { }
+    0x00A => data16_blk_cnt: u16 { }
+    0x00C => response0: u16 { write_bits = 0; }
+    0x00E => response1: u16 { write_bits = 0; }
+    0x010 => response2: u16 { write_bits = 0; }
+    0x012 => response3: u16 { write_bits = 0; }
+    0x014 => response4: u16 { write_bits = 0; }
+    0x016 => response5: u16 { write_bits = 0; }
+    0x018 => response6: u16 { write_bits = 0; }
+    0x01A => response7: u16 { write_bits = 0; }
+    0x01C => irq_status0: u16 { }
+    0x01E => irq_status1: u16 { }
+    0x020 => irq_mask0: u16 { }
+    0x022 => irq_mask1: u16 { }
+    0x024 => clk_ctl: u16 { }
+    0x026 => data16_blk_len: u16 { }
+    0x028 => card_option: u16 { }
+    0x02C => err_status0: u16 { }
+    0x02E => err_status1: u16 { }
+    0x030 => data16_fifo: u16 { }
+    0x0D8 => data16_ctl: u16 { }
+    0x0E0 => software_reset: u16 { write_bits = 0b1; }
+    0x0F6 => protected: u16 { }
+    0x100 => data32_ctl: u16 { }
+    0x104 => data32_blk_len: u16 { }
+    0x108 => data32_blk_cnt: u16 { }
+    0x10C => data32_fifo: u16 { }
 });
