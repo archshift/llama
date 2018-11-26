929f5a123aebd51b5fe10b4b9cffbe2ecb3516b9
Gui Andrade <gh@archshift.com>
2018-11-26 21:54:03 +0000

    build.rs: Fail on decoder-gen failure

--- libllama/build.rs
+++ libllama/build.rs
@@ -14,12 +14,20 @@ fn main() {
         let filename = Path::new(decoder).file_name().unwrap();
         let out = format!("{}/{}.rs", out_dir, filename.to_os_string().to_str().unwrap());
 
-        Command::new("python3")
+        let decoder_stat = Command::new("python3")
             .arg("tools/decoder-gen/decoder-gen.py")
             .arg(decoder)
-            .stdout(File::create(out).unwrap())
-            .spawn()
+            .stdout(File::create(&out).unwrap())
+            .output()
             .expect("failed to execute child");
+
+        if !decoder_stat.status.success() {
+            use std::io::{stderr, Write};
+
+            eprintln!("ERROR: decoder generation failed on {}!", out);
+            eprintln!("Script stderr:");
+            stderr().write_all(&decoder_stat.stderr).unwrap();
+        }
         
         println!("cargo:rerun-if-changed={}", decoder);
     }
