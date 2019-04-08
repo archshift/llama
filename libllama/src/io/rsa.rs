95da40fff7bdbd0e98f3ab99f291db64eda6804a
Gui Andrade <gh@archshift.com>
2019-04-07 20:57:11 -0700

    RSA: Fix exponent FIFO size and key ready bit behavior

--- libllama/src/io/rsa.rs
+++ libllama/src/io/rsa.rs
@@ -1,5 +1,4 @@
 use std::fmt;
-use std::mem;
 
 use openssl::bn;
 
@@ -17,24 +16,29 @@ bf!(RegSlotCnt[u32] {
 
 pub struct RsaKeyslot {
     write_pos: usize,
-    buf: [u8; 0x100]
+    buf: [u8; 0x100],
+    modulus: [u8; 0x100],
+    ready: bool,
 }
 
 impl Clone for RsaKeyslot {
     fn clone(&self) -> RsaKeyslot {
         let mut new_buf = [0u8; 0x100];
-        new_buf.copy_from_slice(&self.buf[..]);
+        new_buf.copy_from_slice(&self.buf);
+        let mut new_mod = [0u8; 0x100];
+        new_mod.copy_from_slice(&self.modulus);
 
         RsaKeyslot {
             write_pos: self.write_pos,
-            buf: new_buf
+            buf: new_buf,
+            modulus: new_mod,
+            ready: self.ready
         }
     }
 }
 
 pub struct RsaDeviceState {
     slots: [RsaKeyslot; 4],
-    modulus: [u8; 0x100],
     message: [u8; 0x100],
 }
 
@@ -42,12 +46,13 @@ impl Default for RsaDeviceState {
     fn default() -> RsaDeviceState {
         let new_keyslot = RsaKeyslot {
             write_pos: 0,
-            buf: [0; 0x100],
+            buf: [0u8; 0x100],
+            modulus: [0u8; 0x100],
+            ready: false,
         };
         RsaDeviceState {
             slots: [new_keyslot.clone(), new_keyslot.clone(),
                     new_keyslot.clone(), new_keyslot],
-            modulus: [0; 0x100],
             message: [0; 0x100],
         }
     }
@@ -69,12 +74,26 @@ fn get_keydata(dev: &RsaDevice, keyslot: usize) -> (RegSlotCnt::Bf, u32) {
     }
 }
 
+fn reg_slot_cnt_read(dev: &mut RsaDevice, keyslot: usize) {
+    let cnt = match keyslot {
+        0 => dev.slot0_cnt.ref_mut(),
+        1 => dev.slot1_cnt.ref_mut(),
+        2 => dev.slot2_cnt.ref_mut(),
+        3 => dev.slot3_cnt.ref_mut(),
+        _ => unreachable!()
+    };
+    let ready = dev._internal_state.slots[keyslot].ready;
+    trace!("Reading from RSA keyslot {} CNT; ready == {:?}", keyslot, ready);
+    RegSlotCnt::alias_mut(cnt).key_set.set(ready as u32);
+}
+
 fn reg_slot_cnt_update(dev: &mut RsaDevice, keyslot: usize) {
     let (slot_cnt, _) = get_keydata(dev, keyslot);
-    if slot_cnt.key_set.get() == 1 {
-        assert_eq!(dev._internal_state.slots[keyslot].write_pos, 0x100);
-    } else {
+    let prev_ready = dev._internal_state.slots[keyslot].ready;
+    if prev_ready && slot_cnt.key_set.get() == 0 {
+        dev._internal_state.slots[keyslot].ready = false;
         dev._internal_state.slots[keyslot].write_pos = 0;
+        trace!("Reset RSA keyslot {}!", keyslot);
     }
 }
 
@@ -109,17 +128,18 @@ fn reg_cnt_update(dev: &mut RsaDevice) {
 
     if cnt.busy.get() == 1 {
         let keyslot = cnt.keyslot.get() as usize;
-        let (slot_cnt, _) = get_keydata(dev, keyslot);
-        assert_eq!(slot_cnt.key_set.get(), 1);
+        let keyslot_data = &dev._internal_state.slots[keyslot];
 
-        info!("Performing RSA arithmetic!");
+        assert!(keyslot_data.ready);
+
+        trace!("Performing RSA arithmetic!");
 
         let mut base_buf = [0u8; 0x100];
         base_buf.copy_from_slice(&dev._internal_state.message[..]);
         let mut exponent_buf = [0u8; 0x100];
-        exponent_buf.copy_from_slice(&dev._internal_state.slots[keyslot].buf[..]);
+        exponent_buf.copy_from_slice(&keyslot_data.buf[..]);
         let mut modulus_buf = [0u8; 0x100];
-        modulus_buf.copy_from_slice(&dev._internal_state.modulus[..]);
+        modulus_buf.copy_from_slice(&keyslot_data.modulus[..]);
 
         if cnt.little_endian.get() == 0 {
             byte_swap_inner(&mut modulus_buf);
@@ -165,14 +185,22 @@ fn reg_cnt_update(dev: &mut RsaDevice) {
 }
 
 fn reg_mod_read(dev: &mut RsaDevice, buf_pos: usize, dest: &mut [u8]) {
-    trace!("Reading {} bytes from RSA MOD at +0x{:X}", dest.len(), buf_pos);
-    let src_slice = &dev._internal_state.modulus[buf_pos .. buf_pos + dest.len()];
+    let cnt = RegCnt::new(dev.cnt.get());
+    let keyslot = cnt.keyslot.get() as usize;
+    trace!("Reading {} bytes from RSA keysot {} MOD at +0x{:X}", dest.len(), keyslot, buf_pos);
+
+    let mod_buf = &dev._internal_state.slots[keyslot].modulus;
+    let src_slice = &mod_buf[buf_pos .. buf_pos + dest.len()];
     dest.clone_from_slice(src_slice);
 }
 
 fn reg_mod_write(dev: &mut RsaDevice, buf_pos: usize, src: &[u8]) {
-    trace!("Writing {} bytes to RSA MOD at +0x{:X}", src.len(), buf_pos);
-    let dst_slice = &mut dev._internal_state.modulus[buf_pos .. buf_pos + src.len()];
+    let cnt = RegCnt::new(dev.cnt.get());
+    let keyslot = cnt.keyslot.get() as usize;
+    trace!("Writing {} bytes to RSA keyslot {} MOD at +0x{:X}", src.len(), keyslot, buf_pos);
+
+    let mod_buf = &mut dev._internal_state.slots[keyslot].modulus;
+    let dst_slice = &mut mod_buf[buf_pos .. buf_pos + src.len()];
     dst_slice.clone_from_slice(src);
 }
 
@@ -188,27 +216,33 @@ fn reg_txt_write(dev: &mut RsaDevice, buf_pos: usize, src: &[u8]) {
     dst_slice.clone_from_slice(src);
 }
 
-fn reg_exp_fifo_write(dev: &mut RsaDevice) {
+fn reg_exp_write(dev: &mut RsaDevice, _: usize, src: &[u8]) {
     let cnt = RegCnt::new(dev.cnt.get());
     let keyslot = cnt.keyslot.get() as usize;
     let (slot_cnt, _) = get_keydata(dev, keyslot);
 
-    assert_eq!(slot_cnt.key_set.get(), 0);
     assert_eq!(slot_cnt.key_prot.get(), 0);
 
-    let write_pos = dev._internal_state.slots[keyslot].write_pos;
-    if write_pos == 0 { // Just starting to update key, clear previous key
-        for b in dev._internal_state.slots[keyslot].buf.iter_mut() {
-            *b = 0;
-        }
+    let keyslot_state = &mut dev._internal_state.slots[keyslot];
+    if keyslot_state.write_pos == 0 { // Just starting to update key, clear previous key
+        keyslot_state.buf = [0u8; 0x100];
     }
-    let word_bytes: [u8; 4] = unsafe { mem::transmute(dev.exp_fifo.get()) };
-    dev._internal_state.slots[keyslot].buf[write_pos .. write_pos + 4].copy_from_slice(&word_bytes[..]);
+
+    let write_pos = keyslot_state.write_pos;
+    assert!(write_pos < 0x100);
+    assert!(src.len() == 4);
+
+    keyslot_state.buf[write_pos .. write_pos + 4].copy_from_slice(&src[..]);
 
     trace!("Writing bytes {:02X},{:02X},{:02X},{:02X} to RSA exponent FIFO!",
-        word_bytes[0], word_bytes[1], word_bytes[2], word_bytes[3]);
+        src[0], src[1], src[2], src[3]);
 
-    dev._internal_state.slots[keyslot].write_pos += 4;
+    keyslot_state.write_pos += 4;
+
+    if (keyslot_state.write_pos == 0x100) {
+        trace!("Committing RSA keyslot {} exponent", keyslot);
+        keyslot_state.ready = true;
+    }
 }
 
 iodevice!(RsaDevice, {
@@ -216,18 +250,40 @@ iodevice!(RsaDevice, {
     regs: {
         0x000 => cnt: u32 { write_effect = reg_cnt_update; }
         0x0F0 => unk: u32 { }
-        0x100 => slot0_cnt: u32 { write_effect = |dev: &mut RsaDevice| reg_slot_cnt_update(dev, 0); }
-        0x104 => slot0_len: u32 { }
-        0x110 => slot1_cnt: u32 { write_effect = |dev: &mut RsaDevice| reg_slot_cnt_update(dev, 1); }
-        0x114 => slot1_len: u32 { }
-        0x120 => slot2_cnt: u32 { write_effect = |dev: &mut RsaDevice| reg_slot_cnt_update(dev, 2); }
-        0x124 => slot2_len: u32 { }
-        0x130 => slot3_cnt: u32 { write_effect = |dev: &mut RsaDevice| reg_slot_cnt_update(dev, 3); }
-        0x134 => slot3_len: u32 { }
-        0x200 => exp_fifo: u32 { write_effect = reg_exp_fifo_write; }
+        0x100 => slot0_cnt: u32 {
+            read_effect  = |dev: &mut RsaDevice| reg_slot_cnt_read(dev, 0);
+            write_effect = |dev: &mut RsaDevice| reg_slot_cnt_update(dev, 0);
+        }
+        0x104 => slot0_len: u32 {
+            default = 0x40;
+        }
+        0x110 => slot1_cnt: u32 {
+            read_effect  = |dev: &mut RsaDevice| reg_slot_cnt_read(dev, 1);
+            write_effect = |dev: &mut RsaDevice| reg_slot_cnt_update(dev, 1);
+        }
+        0x114 => slot1_len: u32 {
+            default = 0x40;
+        }
+        0x120 => slot2_cnt: u32 {
+            read_effect  = |dev: &mut RsaDevice| reg_slot_cnt_read(dev, 2);
+            write_effect = |dev: &mut RsaDevice| reg_slot_cnt_update(dev, 2);
+        }
+        0x124 => slot2_len: u32 {
+            default = 0x40;
+        }
+        0x130 => slot3_cnt: u32 {
+            read_effect  = |dev: &mut RsaDevice| reg_slot_cnt_read(dev, 3);
+            write_effect = |dev: &mut RsaDevice| reg_slot_cnt_update(dev, 3);
+        }
+        0x134 => slot3_len: u32 {
+            default = 0x40;
+        }
     }
     ranges: {
-        0x204;0xFC => { } // A bug in bootrom causes it to write all over this area
+        0x200;0x100 => {
+            read_effect = |_, _, _| panic!("Attempted read from RSA EXP_FIFO!");
+            write_effect = reg_exp_write;
+        }
         0x400;0x100 => {
             read_effect = reg_mod_read;
             write_effect = reg_mod_write;
